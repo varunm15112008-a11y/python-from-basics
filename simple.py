4dae5638f766128ea31048c2b16896f86cc08a02
Varun M <varun.m15112008@gmail.com>
2026-08-27 19:53:55 +0530

    Modify age calculation to use user-provided current year
    Updated the program to ask for the current year instead of hardcoding it.

---
+++ simple.py
@@ -0,0 +1,12 @@
+
+name = input("What is your name? ")
+
+
+birth_year = int(input("What year were you born? "))
+
+
+current_year = int(input("What is the current year"))
+age = current_year - birth_year
+
+
+print(f"Hello, {name}! You are {age} years old.")
